d6523739e924d01d68981c4178de81f4421fc4a8
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-19 00:12:05 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setuptools.setup(
     install_requires=REQUIRED,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: None",
         "Operating System :: OS Independent",
     ],
 )
