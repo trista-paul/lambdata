02066ba24ee6fad1e0e2ebf328205191027b510e
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-18 22:51:21 -0700

    typo fix

--- lambdata_trista/__init__.py
+++ lambdata_trista/__init__.py
@@ -3,7 +3,7 @@ lambdata - a collection of Data Science helper functions
 """
 
 import pandas as pd
-import nimpy as np
+import numpy as np
 
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
