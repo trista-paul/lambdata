2bd7ccc46e7fc0e491e167ca220e54364e918461
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-19 00:00:26 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+""" lambdata - a collection of Data Science helper functions
+"""
+
+import setuptools
+
+REQUIRED = [
+    "numpy",
+    "pandas",
+    "datetime"
+]
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name="lambdata_trista",
+    version="0.0.1",
+    author="trista-paul",
+    description="A collection of Data Science helper functions",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/trista-paul/lambdata",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=REQUIRED,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: None",
+        "Operating System :: OS Independent",
+    ],
+)
