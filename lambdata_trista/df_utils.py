3d4a06dc23603b77988886bf23ddc1a81eb029fa
trista-paul <44633004+trista-paul@users.noreply.github.com>
2019-03-19 14:31:18 -0700

    Formatted to comply with PEP8

--- lambdata_trista/df_utils.py
+++ lambdata_trista/df_utils.py
@@ -2,24 +2,40 @@
 utility functions for working with dataframes.
 """
 
-import pandas
 
-TEST_DF = pandas.DataFrame([1,2,3])
+import pandas as pd
+import numpy as np
+import os
+
+
+"""import, load dataset from UCI with datetime, string and int columns"""
+os.system("wget http://archive.ics.uci.edu/ml/machine-learning-databases/00482/dataset.zip")
+os.system("unzip dataset.zip")
+TEST_DF = pd.read_csv('dataset.csv')
+
 
 def outlier_cleaner(df):
-  for label, col in df.iteritems():
-    Q1 = col.quantile(0.25)
-    Q3 = col.quantile(0.75)
-    IQR = Q3 - Q1
-    df = df[((Q1 -(IQR*1.5)) <= col) & (col <= (Q3 +(IQR*1.5)))]
-  return df
-  
-def datetime_splitter(df, col): #accepts DataFrame and time column
-  import datetime
-  df[col] = pd.to_datetime(df[col])
-  df['year'] = df[col].dt.year
-  df['month'] = df[col].dt.month
-  df['day'] = df[col].dt.day
-  df = df.drop(columns = col)
-  return df
-  
+    """removes float more than Q3+IQR*1.5 or less than Q1-IQR*1.5"""
+    """accepts DataFrame"""
+    num = df.select_dtypes(include=np.number)
+    for label, col in num.iteritems():
+        Q1 = col.quantile(0.25)
+        Q3 = col.quantile(0.75)
+        IQR = Q3 - Q1
+        clean = num[((Q1 - (IQR*1.5)) <= col) & (col <= (Q3 + (IQR*1.5)))]
+        df = pd.concat([df, clean], axis=1)
+        df = df.dropna()
+        df = df.drop(columns=num)
+    return df
+
+
+def datetime_splitter(df, col):
+    """splits date format column into year, month, day columns"""
+    """accepts DataFrame and date format column"""
+    import datetime
+    if col.dtype != np.datetime64:
+        col = pd.to_datetime(col)
+    df['year'] = col.dt.year
+    df['month'] = col.dt.month
+    df['day'] = col.dt.day
+    return df
